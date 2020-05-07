1c8dea6b9ced134e5349bf87b54a7a60dab9d7cb
kingoftheconnors <connormcp777@gmail.com>
2020-05-07 12:17:42 -0400

    Different betas

--- convTilesetMaker.py
+++ convTilesetMaker.py
@@ -16,7 +16,8 @@ import random
 randomGen = np.random.default_rng()
 
 p = {
-    'lambda': [0.00001, 0.000005, 0.000001],
+    'beta1': [0.5, 0.7, 0.8],
+    'beta2': [0.8, 0.9, 0.95, 0.99],
 }
 
 # Import images
@@ -87,49 +88,49 @@ def my_model(x_train, y_train, x_val, y_val, params):
 
     # A: 256 -> 128
     A = Conv2D(filter_size, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(input_img)
+        activity_regularizer=l2(1e-6))(input_img)
     A = LeakyReLU(alpha=0.2)(A)
     A = BatchNormalization(momentum=0.8)(A)
     A = Dropout(0.2)(A)
 
     # B: 128 -> 64
     B = Conv2D(filter_size*2, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(A)
+        activity_regularizer=l2(1e-6))(A)
     B = LeakyReLU(alpha=0.2)(B)
     B = BatchNormalization(momentum=0.8)(B)
     B = Dropout(0.2)(B)
 
     # C: 64 -> 32
     C = Conv2D(filter_size*2, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(B)
+        activity_regularizer=l2(1e-6))(B)
     C = LeakyReLU(alpha=0.2)(C)
     C = BatchNormalization(momentum=0.8)(C)
     #C = Dropout(0.2)(C) #
 
     # D: 32 -> 16
     D = Conv2D(filter_size*4, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(C)
+        activity_regularizer=l2(1e-6))(C)
     D = LeakyReLU(alpha=0.2)(D)
     D = BatchNormalization(momentum=0.8)(D)
     D = Dropout(0.2)(D)
 
     # E: 16 -> 8
     E = Conv2D(filter_size*4, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(D)
+        activity_regularizer=l2(1e-6))(D)
     E = LeakyReLU(alpha=0.2)(E)
     E = BatchNormalization(momentum=0.8)(E)
     E = Dropout(0.2)(E) #
 
     # F: 8 -> 4
     F = Conv2D(filter_size*4, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(E)
+        activity_regularizer=l2(1e-6))(E)
     F = LeakyReLU(alpha=0.2)(F)
     F = BatchNormalization(momentum=0.8)(F)
     #F = Dropout(0.2)(F)
 
     # G: 4 -> 2
     G = Conv2D(filter_size*4, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(F)
+        activity_regularizer=l2(1e-6))(F)
     G = LeakyReLU(alpha=0.2)(G)
     G = BatchNormalization(momentum=0.8)(G)
     G = Dropout(0.2)(G)
@@ -137,7 +138,7 @@ def my_model(x_train, y_train, x_val, y_val, params):
     # 2 -> 4
     #upG = concatenate([upscale, G])
     upG = Conv2DTranspose(filter_size*8, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(G)
+        activity_regularizer=l2(1e-6))(G)
     upG = LeakyReLU(alpha=0.2)(upG)
     upG = BatchNormalization(momentum=0.8)(upG)
     #upG = Dropout(0.2)(upG) #
@@ -145,7 +146,7 @@ def my_model(x_train, y_train, x_val, y_val, params):
     # 4 -> 8
     upF = concatenate([upG, F])
     upF = Conv2DTranspose(filter_size*4, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(upF)
+        activity_regularizer=l2(1e-6))(upF)
     upF = BatchNormalization(momentum=0.8)(upF)
     upF = Activation('relu')(upF)
     #upF = Dropout(0.2)(upF)
@@ -153,7 +154,7 @@ def my_model(x_train, y_train, x_val, y_val, params):
     # E: 8 -> 16
     upE = concatenate([upF, E])
     upE = Conv2DTranspose(filter_size*2, (3, 3), strides=2, padding='same',
-        activity_regularizer=l2(params['lambda']))(upE)
+        activity_regularizer=l2(1e-6))(upE)
     upE = BatchNormalization(momentum=0.8)(upE)
     upE = Activation('relu')(upE)
     #upE = Dropout(0.2)(upE) #
@@ -161,13 +162,13 @@ def my_model(x_train, y_train, x_val, y_val, params):
     # C: 16 -> 48
     upD = concatenate([upE, D])
     upD = Conv2DTranspose(3, (3, 3), strides=3, padding='same',
-        activity_regularizer=l2(params['lambda']))(upD)
+        activity_regularizer=l2(1e-6))(upD)
     upD = Activation('sigmoid')(upD)
 
     model = Model(input_img, upD)
     model.summary()
 
-    opt = Adam(beta_1=0.5)
+    opt = Adam(beta_1=params['beta1'], beta_2=params['beta2'])
     model.compile(optimizer=opt, loss='mse')#, metrics=['accuracy'])
     # fits the model on batches with real-time data augmentation:
     #es = EarlyStopping(monitor='val_loss', min_delta=0, patience=15, verbose=0, mode='auto')
@@ -180,7 +181,7 @@ def my_model(x_train, y_train, x_val, y_val, params):
                     epochs=200,
                     verbose = 1, callbacks=[reduce_lr])
 
-    model.save('tilesetmaker-' + "after" + str(params['lambda']) + '.h5')
+    model.save('tilesetmaker-' + "after" + str(params['beta1']) + " " + str(params['beta2']) + '.h5')
 
     #  "Loss"
     plt.plot(out.history['loss'])
@@ -190,7 +191,7 @@ def my_model(x_train, y_train, x_val, y_val, params):
     plt.gca().set_ylim([0,1])
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig("metrics/loss-after %s.png" % (str(params['lambda'])))
+    plt.savefig("metrics/loss-after %s.png" % (str(params['beta1']*100) + " " + str(params['beta2']*100) ))
     plt.clf()
     
     #_, axs = plt.subplots(10, 2)
@@ -217,4 +218,4 @@ talos.Scan(x_train, y_train, p, my_model, x_val=x_validation, y_val=y_validation
 # decreasing filter size MORE
 # GAN?
 # ReduceLROnPlateau when val_loss plateaus to go from macro learning to fine-tuning what validation error plateaus
-#               (Less patience, lower plateau rate, etc?)
+#               (Less patience, lower plateau factor, etc?)
--- convTilesetUser.py
+++ convTilesetUser.py
@@ -15,12 +15,23 @@ model2 = load_model('tilesetmaker-before1e-05.h5')
 model3 = load_model('tilesetmaker-before5e-06.h5')
 model4 = load_model('tilesetmaker-before1e-06.h5')
 
-model5 = load_model('tilesetmaker-after1e-05.h5')
-model6 = load_model('tilesetmaker-after5e-06.h5')
-model7 = load_model('tilesetmaker-after1e-06.h5')
+model5 = load_model('tilesetmaker-after0.8 0.99.h5')
+model6 = load_model('tilesetmaker-after0.8 0.95.h5')
+model7 = load_model('tilesetmaker-after0.8 0.9.h5')
+model8 = load_model('tilesetmaker-after0.8 0.8.h5')
+
+model9 = load_model('tilesetmaker-after0.7 0.99.h5')
+model10 = load_model('tilesetmaker-after0.7 0.95.h5')
+model11 = load_model('tilesetmaker-after0.7 0.9.h5')
+model12 = load_model('tilesetmaker-after0.7 0.8.h5')
+
+model13 = load_model('tilesetmaker-after0.5 0.99.h5')
+model14 = load_model('tilesetmaker-after0.5 0.95.h5')
+model15 = load_model('tilesetmaker-after0.5 0.9.h5')
+model16 = load_model('tilesetmaker-after0.5 0.8.h5')
 
 def testImage(filenames):
-    _, axs = plt.subplots(len(filenames), 8)
+    _, axs = plt.subplots(len(filenames), 17)
     for i in range(len(filenames)):
         filename = filenames[i]
         image = Image.open(filename).convert('RGB')
@@ -32,6 +43,15 @@ def testImage(filenames):
         output5 = model5.predict(np.expand_dims(data, axis=0))
         output6 = model6.predict(np.expand_dims(data, axis=0))
         output7 = model7.predict(np.expand_dims(data, axis=0))
+        output8 = model8.predict(np.expand_dims(data, axis=0))
+        output9 = model9.predict(np.expand_dims(data, axis=0))
+        output10 = model10.predict(np.expand_dims(data, axis=0))
+        output11 = model11.predict(np.expand_dims(data, axis=0))
+        output12 = model12.predict(np.expand_dims(data, axis=0))
+        output13 = model13.predict(np.expand_dims(data, axis=0))
+        output14 = model14.predict(np.expand_dims(data, axis=0))
+        output15 = model15.predict(np.expand_dims(data, axis=0))
+        output16 = model16.predict(np.expand_dims(data, axis=0))
         
         axs[i, 0].imshow(data)
         axs[i, 1].imshow(output2[0])
@@ -40,6 +60,15 @@ def testImage(filenames):
         axs[i, 5].imshow(output5[0])
         axs[i, 6].imshow(output6[0])
         axs[i, 7].imshow(output7[0])
+        axs[i, 8].imshow(output8[0])
+        axs[i, 9].imshow(output9[0])
+        axs[i, 10].imshow(output10[0])
+        axs[i, 11].imshow(output11[0])
+        axs[i, 12].imshow(output12[0])
+        axs[i, 13].imshow(output13[0])
+        axs[i, 14].imshow(output14[0])
+        axs[i, 15].imshow(output15[0])
+        axs[i, 16].imshow(output16[0])
     plt.show()
 
 
